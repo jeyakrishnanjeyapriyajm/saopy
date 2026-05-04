1383744e704ae7330a85e71e162e894645e0403c
Jeyakrishnan Jeyapriya <jeyakrishnanjeyapriya31@gmail.com>
2026-05-04 12:07:17 +0530

    Update API_BASE_URL to allow default empty string
    Set default value for API_BASE_URL and remove error log.

--- laundry-new/src/api/contactApi.js
+++ laundry-new/src/api/contactApi.js
@@ -1,10 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-if (!API_BASE_URL) {
-  console.error("⚠️ Backend not connected. Please set VITE_API_BASE_URL.");
-}
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 export const contactApi = createApi({
   reducerPath: "contactApi",
